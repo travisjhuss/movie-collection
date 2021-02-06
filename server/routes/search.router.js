07c67aa80117ddc23bf66a15da8d761f13e9f10a
Travis Huss <travisjhuss@gmail.com>
2021-02-06 13:02:19 -0600

    search sql query adjusted to group movie id's together

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -8,12 +8,13 @@ router.get('/', (req, res) => {
     console.log('GET for search triggered:', req.query);
     const searchText = `%${req.query.q}%`;
     const query = `
-        SELECT * FROM "movies" 
+        SELECT "movies".id, "movies".title, "movies".poster, "movies".description FROM "movies" 
         JOIN "movies_genres" on "movies".id = "movies_genres".movie_id
         JOIN "genres" on "movies_genres".genre_id = "genres".id
         WHERE "movies".title ILIKE $1 OR 
         "genres".name ILIKE $1 OR
-        "movies".description ILIKE $1;
+        "movies".description ILIKE $1
+        GROUP BY "movies".id;
     `;
     pool.query(query, [searchText])
         .then(result => {
